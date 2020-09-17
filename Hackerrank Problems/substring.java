3930ba75077b58aa31598c145077bc5ce4229083
Siddharth Singh <55332990+sidsinghrajput@users.noreply.github.com>
2020-09-18 01:30:08 +0530

    Update and rename substring.java to Hackerrank Problems/substring.java

--- Hackerrank Problems/substring.java
+++ Hackerrank Problems/substring.java
@@ -1,19 +1,10 @@
 for(int i = 0;i<=s.length()-k;i++){
-
              String subString = s.substring(i,i+k);
-
              if(i == 0){
-
                  smallest = subString;
-
              }
-
              if(subString.compareTo(largest)>0){
-
                  largest = subString;
-
              }else if(subString.compareTo(smallest)<0)
-
                  smallest = subString;
-
       }
